d3b3a8aa794524a3577cb2f923e3bfffca605085
Jose <jocampo2@ucol.mx>
2020-11-26 15:53:43 -0600

    added costoRenta

--- src/main.js
+++ src/main.js
@@ -8,6 +8,21 @@ export default class App {
         }
     }
 
+    costoRenta(kilometros){
+        if(kilometros < 50){
+            return kilometros * 3;  
+        }
+        else if(kilometros >= 50 && kilometros < 100 ){
+            return kilometros * 5;
+        }
+        else if(kilometros >=  100 && kilometros < 200){
+            return kilometros * 6;
+        }
+        else if(kilometros >= 200){
+            return kilometros * 6.5;
+        }
+    }
+
 }
 
 let app = new App();
