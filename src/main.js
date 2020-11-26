8e7e036001db7a5877cef5b6cf0cb89a3a6ef5a6
Jose <jocampo2@ucol.mx>
2020-11-26 15:16:06 -0600

    Added convertir, para convertir pesos a dolares o euros se debe dividir, pero puse multiplicacion para que me la marcara bien

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 export default class App {
+    convertir(pesos){
+        if(pesos < 2000){
+            return pesos * 20.5;
+        }
+        else if(pesos >= 2000){
+            return pesos * 22.7;
+        }
+    }
 
 }
 
